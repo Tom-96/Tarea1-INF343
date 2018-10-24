f333a75b6d0b6f9b5b9d07ad1b3529af5bc9407d
Raurur00 <javierzavalatorres@gmail.com>
2018-10-24 17:31:42 -0300

    Multicast

--- src/Client.java
+++ src/Client.java
@@ -7,16 +7,28 @@ import java.net.UnknownHostException;
 public class Client {
 
     final static String INET_ADDR = "224.0.0.3";
-    final static int PORT = 8888;
+    final static int[] PORT = {8888,8889,8890,8891};
 
     public static void main(String[] args) throws UnknownHostException {
-        // Get the address that we are going to connect to.
+        String ip = args[0]; 
+        String mediciones = args[1];
+        String anteriores = args[2];
+        
+        // Obtención de la dirección a la que nos queremos conectar
         InetAddress address = InetAddress.getByName(INET_ADDR);
         byte[] buf = new byte[256];
+        // Suscripción a variables 
+        for (int i = 0; i < mediciones.length (); i++) { 
+            char c = mediciones.charAt (i);
+            if (c=='1'){
+                //Crear thread
+                System.out.println (c);
+            }
+        }
 
         // Create a new Multicast socket (that will allow other sockets/programs
         // to join it as well.
-        try (MulticastSocket clientSocket = new MulticastSocket(PORT)){
+        try (MulticastSocket clientSocket = new MulticastSocket(PORT[0])){
             //Joint the Multicast group.
             clientSocket.joinGroup(address);
 
@@ -24,7 +36,6 @@ public class Client {
                 // Receive the information and print it.
                 DatagramPacket msgPacket = new DatagramPacket(buf, buf.length);
                 clientSocket.receive(msgPacket);
-
                 String msg = new String(buf, 0, buf.length);
                 System.out.println("Socket 1 received msg: " + msg);
             }
--- src/Server.java
+++ src/Server.java
@@ -6,12 +6,15 @@ import java.net.UnknownHostException;
 
 public class Server {
 
-    final static String INET_ADDR = "224.0.0.3";
+    private static String ip;
     final static int PORT = 8888;
 
     public static void main(String[] args) throws UnknownHostException, InterruptedException {
+        //ip = args[0];
+        ip = "224.0.0.3";
+        System.out.println("ip: " + ip);
         // Get the address that we are going to connect to.
-        InetAddress addr = InetAddress.getByName(INET_ADDR);
+        InetAddress addr = InetAddress.getByName(ip);
 
         // Open a new DatagramSocket, which will be used to send the data.
         try (DatagramSocket serverSocket = new DatagramSocket()) {
@@ -25,7 +28,7 @@ public class Server {
                 serverSocket.send(msgPacket);
 
                 System.out.println("Server sent packet with msg: " + msg);
-                Thread.sleep(500);
+                Thread.sleep(2000);
             }
         } catch (IOException ex) {
             ex.printStackTrace();
